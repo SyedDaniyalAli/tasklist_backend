3bbf4fc4159f139c60b4068a4c13b41078b9b077
Syed Daniyal Ali <SyedDaniyalAli2222@gmail.com>
2023-12-13 23:56:03 +0500

    performed rest apis

---
+++ routes/api/rest_api/index.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:http/http.dart' as http;
+
+Future<Response> onRequest(RequestContext context) {
+  return switch (context.request.method) {
+    HttpMethod.get => _getRecipes(context),
+    _ => Future.value(Response(statusCode: HttpStatus.methodNotAllowed)),
+  };
+}
+
+Future<Response> _getRecipes(RequestContext context) async {
+  final response = await http.get(
+    Uri.parse('https://low-carb-recipes.p.rapidapi.com/random'),
+    headers: {
+      'X-RapidAPI-Key': 'b8849a6c88msha9a7ef62f3ef62ap1d9327jsn03d96a53bc74',
+      'X-RapidAPI-Host': 'low-carb-recipes.p.rapidapi.com',
+    },
+  );
+
+  if (response.statusCode == HttpStatus.ok) {
+    return Response(
+      body: response.body,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+  } else {
+    return Response(statusCode: HttpStatus.internalServerError);
+  }
+}
